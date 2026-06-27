41e41ab4a569da489502ffe2ff8a156a1a8060d1
yee <yee@yees-Mac-mini.local>
2026-06-27 18:01:41 -0500

    feat(movie): animate fireworks launch and burst

--- themovie/assets/audio-BRDJz-bi.js
+++ themovie/assets/audio-BRDJz-bi.js
@@ -1,4 +1,4 @@
-import{n as e,t}from"./index-CExfgWnD.js";var n=`
+import{n as e,t}from"./index-CKWwmkac.js";var n=`
 registerProcessor("aperture-brickwall-limiter", class extends AudioWorkletProcessor {
   process(inputs, outputs) {
     const input = inputs[0];
